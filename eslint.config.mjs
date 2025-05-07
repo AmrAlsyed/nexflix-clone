8cc25e987d6286c75dd658f54ad9b38f053e735e
AmrAlsyed <Xaxeth@gmail.com>
2025-05-07 07:23:25 +0300

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"], // Target TypeScript files
+    rules: {
+      "no-console": "warn", // Warn on console.log
+      "no-unused-vars": "error", // Error on unused variables
+      "react/react-in-jsx-scope": "off", // Disable rule (Next.js doesn't need React in scope)
+      "@next/next/no-img-element": "off", // Disable Next.js rule for <img> elements
+      "@typescript-eslint/no-explicit-any": "off", // or "off"
+      "no-console": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
